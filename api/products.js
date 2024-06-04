0bcc1aff882978f526e4489b4d73c917d1c45834
gabslr <gaby.lopez.rojas@gmail.com>
2024-06-04 11:47:55 -0600

    refactor: Json para hacer referencia a APIs en Vercel

--- api/products.js
+++ api/products.js
@@ -1,28 +1,18 @@
-// api/products.js
-
-const products = [];
+let products = require('../db.json');
 
 export default function handler(req, res) {
-  if (req.method === 'GET') {
-    // Obtiene todos los productos
-    res.status(200).json(products);
-  } else if (req.method === 'POST') {
-    // Agrega un nuevo producto
-    const newProduct = req.body;
-    newProduct.id = products.length + 1;
-    products.push(newProduct);
-    res.status(201).json(newProduct);
-  } else if (req.method === 'DELETE') {
-    // Elimina un producto
-    const { id } = req.query;
-    const index = products.findIndex(p => p.id === parseInt(id, 10));
-    if (index !== -1) {
-      products.splice(index, 1);
-      res.status(204).end();
+    if (req.method === 'GET') {
+        res.status(200).json(products);
+    } else if (req.method === 'POST') {
+        const newProduct = req.body;
+        newProduct.id = products.length + 1;
+        products.push(newProduct);
+        res.status(201).json(newProduct);
+    } else if (req.method === 'DELETE') {
+        const { id } = req.query;
+        products = products.filter(product => product.id !== parseInt(id, 10));
+        res.status(204).end();
     } else {
-      res.status(404).json({ message: 'Product not found' });
+        res.status(405).json({ message: 'Method not allowed' });
     }
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
 }
